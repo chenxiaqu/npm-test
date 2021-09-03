577dd046db6c504d760d3ac237acb7288c1a34a9
pangzhaozhao <pangzhaozhao@hd123.com>
2021-09-03 18:32:44 +0800

    loading组件

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 庞昭昭
+ * @Date: 2021-09-03 15:16:00
+ * @LastEditTime: 2021-09-03 17:32:57
+ * @LastEditors: 庞昭昭
+ * @Description: 
+ * @FilePath: \npm\index.js
+ * 记得注释
+ */
+const Ploading = require("./lib/ploading.js");
+
+module.exports = {
+  Ploading
+};
